a54766b0aa32f738202c4853c2b6a2d588761041
maicol <maykolandres4@gmail.com>
2022-11-13 04:13:04 -0500

    done with home desktop

---
+++ components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link  from 'next/link'
+
+const NavBar = () => {
+  return (
+    <div className='nav-bar'>
+        <div className='nav-logo'>
+        <h3>Euonia</h3>
+        </div>
+       <div className='nav-links'>
+       <Link href="/" className='link'>Home</Link>
+        <Link href="/about" className='link'>About</Link>
+        <Link href="/ninjas" className='link'>Ninjas</Link>
+       </div>
+    </div>
+  )
+}
+
+export default NavBar
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,38 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div>
-    <h1>euonia</h1>
+     <Head>
+    <title>Euonia | Home</title>
+    <meta name='keywords' content="" />
+   </Head>
+    <div className='home'>
+      <div className='layer'>
+
+        <div className='home-container'>
+         <div className='home-box-title'>
+         <h1 className='home-title'>Euonia</h1>
+          <div className='home-text'>
+            <p>[u-noy-a] Greek</p>
+            <p>(n.) a pure and well balanced mind, a good spirit.
+            </p>
+            <span>Beautiful thinking</span>
+            <h3>Discover and learn about ancient Greek and Roman art, their history, places, and Gods.  
+             </h3>
+          </div>
+         </div>
+
+         <div className='home-box'/>
+         
+         <div className='home-img'>
+         <Image src="/img.png" width={510} height={700} className="home-hero"/>
+         </div>
+        </div>
+
+      </div>
+    </div>
     </div>
   )
 }
