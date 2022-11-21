eb2713da9a108e985c5d4d939b5f158e4a6492b3
maicol <maykolandres4@gmail.com>
2022-11-21 17:18:16 -0500

    finished euonia v1

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import Link  from 'next/link'
+import { useState } from 'react';
+import { GiHamburgerMenu} from 'react-icons/gi'
+import {MdClose} from  'react-icons/md'
+
 
 const NavBar = () => {
+
+  const [showMenu, setShowMenu] = useState(false);
+
+let menu;
+
+if(showMenu){
+  menu = <div >
+
+  </div>
+}
+
+
   return (
     <div className='nav-bar'>
         <div className='nav-logo'>
@@ -12,6 +28,29 @@ const NavBar = () => {
         <Link href="/languages" className='link'>Languages</Link>
         <Link href="/art" className='link'>Art & Sculptures</Link>
        </div>
+
+       <div className='menu-mobile'>
+
+       <div  className='mobile-btn'   onClick={()=> setShowMenu(!showMenu)}><GiHamburgerMenu className='mobile-btn-icon'/></div>
+       { menu ? 
+      <>
+    
+
+        <div className='mobile-menu-container'>
+        <div className='mobile-btn-close' onClick={()=> setShowMenu(false)}><MdClose className='mobile-icon-close'/> </div>
+        <Link href="/" className='link'>Home</Link>
+        <Link href="/languages" className='link'>Languages</Link>
+        <Link href="/art" className='link'>Art & Sculptures</Link>
+
+        </div>
+    
+      </>
+         : false}
+
+
+       </div>
+
+
     </div>
   )
 }
